d1bb399fc46e8c7f86009715cb2c8c86be542a0f
h2ero <122750707@qq.com>
2016-05-26 22:22:24 +0800

    update

--- keynav/AppDelegate.swift
+++ keynav/AppDelegate.swift
@@ -10,11 +10,15 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+     @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+         window.opaque = false;
+        let bgColor: NSColor = NSColor(red: 1.0, green: 0.1, blue: 0.1, alpha: 0.2);
+        
+        window.backgroundColor = bgColor;
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
--- keynav/ViewController.swift
+++ keynav/ViewController.swift
@@ -7,21 +7,51 @@
 //
 
 import Cocoa
+import AppKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController  {
 
+    @IBOutlet var gradView: NSView!
     override func viewDidLoad() {
         super.viewDidLoad()
+            self.view.wantsLayer = true
+
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear() {
+        gradView.layer?.backgroundColor = NSColor.clearColor().CGColor
+        //box.layer?.setNeedsDisplay()
     }
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
+        gradView.layer?.backgroundColor = NSColor.blueColor().CGColor
+            let backgroundColor = NSColor.whiteColor()
+        backgroundColor.setFill()
+        NSRectFill(NSMakeRect(0, 20, 300.0, 178.0))
+        
+        NSColor.redColor().setStroke()
+        NSColor.magentaColor().setFill()
+        //空のベジェパスを作成
+        let aBezier : NSBezierPath = NSBezierPath()
+        aBezier.moveToPoint(CGPoint(x: 176.95,y: 44.90))
+        aBezier.curveToPoint(CGPoint(x: 166.71,y: 145.89),
+                             controlPoint1: CGPoint(x: 76.63,y: 76.78),
+                             controlPoint2: CGPoint(x: 82.59,y: 206.70))
+        aBezier.curveToPoint(CGPoint(x: 176.95,y: 44.90),
+                             controlPoint1: CGPoint(x: 237.55,y: 224.76),
+                             controlPoint2: CGPoint(x: 276.83,y: 95.98))
+        aBezier.closePath()
+        aBezier.fill()
+        aBezier.lineWidth = 2.0
+        aBezier.stroke()
+        
         }
     }
-
+    
 
 }
 
