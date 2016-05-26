11789b46aeff9f907a4b461c1dd06a129723e61a
h2ero <122750707@qq.com>
2016-05-26 15:16:08 +0800

    Initial Commit

---
+++ keynav/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  keynav
+//
+//  Created by h2ero on 5/26/16.
+//  Copyright © 2016 h2ero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ keynav/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  keynav
+//
+//  Created by h2ero on 5/26/16.
+//  Copyright © 2016 h2ero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
