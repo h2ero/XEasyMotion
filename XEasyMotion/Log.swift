a347859a0f34a140c11e0488c53ec097f36b6696
h2ero <122750707@qq.com>
2017-10-12 16:53:04 +0800

    update to latest swift version

--- XEasyMotion/Log.swift
+++ XEasyMotion/Log.swift
@@ -9,30 +9,30 @@
 import Foundation
 class Log {
     static let (ERROR,INFO) = ("Error",  "Info")
-    static let mainBundle = NSBundle.mainBundle()
+    static let mainBundle = Bundle.main
     static let bundleID = mainBundle.bundleIdentifier! as String
     static let bundleName = mainBundle.infoDictionary!["CFBundleName"]
     static let bundleVersion = mainBundle.infoDictionary!["CFBundleShortVersionString"]
     //    static let tempDirectory = NSTemporaryDirectory()
     static let tempDirectory = "/tmp/"
-    static var logName = Log.tempDirectory.stringByAppendingString("\(Log.bundleID).log")
+    static var logName = Log.tempDirectory + ("\(Log.bundleID).log")
     
     
     static func write(errLevel:String, catelog:String, value:AnyObject) {
-        let value = "[\(errLevel)][\(catelog)] " + String(value)
-        if Constents.debug == false {
-            NSLog(value)
-            return
-        }
-        let formatter = NSDateFormatter()
+        let value = "[\(errLevel)][\(catelog)] " + String(describing: value)
+   
+        let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         NSLog(logName)
-        if let outputStream = NSOutputStream(toFileAtPath: logName, append: true) {
+        if let outputStream = OutputStream(toFileAtPath: logName, append: true) {
             outputStream.open()
-            let text = "\(formatter.stringFromDate(NSDate())) \(Log.bundleID)-\(NSDate().timeIntervalSince1970) \(value)\n"
-            let data = text.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
-            outputStream.write(UnsafePointer(data.bytes), maxLength: data.length)
+            let text = "\(formatter.string(from: NSDate() as Date)) \(Log.bundleID)-\(NSDate().timeIntervalSince1970) \(value)\n"
+//            let data = text.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+//            outputStream.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length)
+            let data: NSData = text.data(using: String.Encoding.utf8)! as NSData
+            let p: UnsafePointer = data.bytes.assumingMemoryBound(to: UInt8.self)
+            outputStream.write(p, maxLength: data.length)
             outputStream.close()
         }
         NSLog(value)
